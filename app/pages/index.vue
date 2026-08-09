97d1087dda8c6e201ef0fc285383c70d8457b540
ilhambonardoo <ilhambonardo123@gmail.com>
2026-08-09 19:10:19 +0700

    (ADD) --> Membuat tampilan card untuk info cafe

---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<!-- pages/index.vue -->
+<template>
+  <div class="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+    <CafeCard
+      name="Kopi Nako Bogor"
+      address="Jl. Pajajaran Indah V No.7, Bogor Timur"
+      image-url="https://images.unsplash.com/photo-1554118811-1e0d58224f24"
+      :is-open24h="true"
+      :price-range="2"
+      :facilities="['WiFi Kencang', 'Banyak Colokan', 'Outdoor']"
+      @select-map="handleFocusMap"
+    />
+  </div>
+</template>
+
+<script setup>
+const handleFocusMap = () => {
+  console.log("Fokuskan peta ke koordinat cafe ini!");
+};
+</script>
